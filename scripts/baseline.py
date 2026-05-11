ea6800816e32c2919d25cfe716bce1567387a5f3
Devon Vorster <devo@telus.net>
2026-05-11 15:44:59 -0700

    Add minimal working scripts to pipeline

---
+++ scripts/baseline.py
@@ -0,0 +1,13 @@
+"""
+Baseline approach to detecting cross-sucking events bewteen dairy calves.
+Uses YOLOv8 to track calves and signals cross-sucking events at threshold overlap.
+"""
+
+
+def main():
+    # Technical execution logic goes here
+    print("Run baseline detection model here...")
+
+
+if __name__ == "__main__":
+    main()
