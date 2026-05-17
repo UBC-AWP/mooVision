23315085c2e1d02a127c25f527f8b284f01add40
rw1030 <rw1030@student.ubc.ca>
2026-05-17 13:30:44 -0700

    add dir for baseline json

--- config.py
+++ config.py
@@ -14,13 +14,28 @@ def require_dir(var: str) -> Path:
 
 load_dotenv()  
 
+# Path on OneDrive shared library (syncs with local)
 ROOT_DIR = require_dir("ROOT_DIR")
 
-# Path on OneDrive shared library (syncs with local)
-ROOT_DIR = Path(os.environ["ROOT_DIR"])
+# Path for clips and index
 CLIPS_DIR = ROOT_DIR / "cross_sucking_clips"
+CLIPS_INDEX_DIR = CLIPS_DIR / "all_clips_index.csv"
+
+# Path for raw videos
 RAW_DIR = ROOT_DIR / "raw_cross_sucking_datalog"
-CLIPS_INDEX_PATH = CLIPS_DIR / "all_clips_index.csv"
+
+# Path for data folder 
 DATA_FOLDER_DIR = ROOT_DIR / "data"
+
+# Directory to save the metadata of the clips
 CLIPS_METADATA_DIR = DATA_FOLDER_DIR / "clips_metadata"
 CLIPS_METADATA_DIR.mkdir(parents=True, exist_ok=True)
+
+# Path to the output of the reproduced clips
+REPRODUCED_CLIPS_DIR = ROOT_DIR / "reproduced_clips"
+REPRODUCED_CLIPS_DIR.mkdir(parents=True, exist_ok=True)
+
+# Path on GitHub
+LOCAL_DIR = require_dir("LOCAL_DIR")
+# Path to baseline metadata output
+BASELINE_METADATA_DIR = LOCAL_DIR / "results" / "metadata" / "baseline" / "CS_0001_POSTWEAN_d1_p2_cow6_02112025_ch02-20251102075200_684_702_results.json"
