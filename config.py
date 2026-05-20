ffb79a6bc1a1cd7f781f2e9f61569d7cc9dbd57c
Devon Vorster <devo@telus.net>
2026-05-19 21:42:39 -0700

    Add video paths to config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 ROOT = Path(os.environ["MOOVISION_CLIPS_DIR"])
@@ -8,5 +9,9 @@ TESTROOT = Path(os.environ["TEST_DIR"])
 
 DATA_DIR = ROOT.parent / "data"
 METADATA_DIR = DATA_DIR / "metadata"
+
+UNLABELLED_CLIPS_DIR = ROOT / "cross_sucking_clips"
+LABELLED_CLIPS_DIR = ROOT / "cross_sucking_labelled"
+SOURCE_VIDEOS_DIR = ROOT / "raw_cross_sucking_datalog" / "videos"
 
 METADATA_DIR.mkdir(parents=True, exist_ok=True)
