474366fa3ef32ea949c1eafa17058af69eb3bf6e
Devon Vorster <devo@telus.net>
2026-05-26 11:34:19 -0700

    update config wil with Paths to index csv and processed csv

--- config.py
+++ config.py
@@ -23,6 +23,10 @@ load_dotenv()
 # Path on OneDrive shared library (syncs with local)
 ROOT_DIR = require_dir("ROOT_DIR")
 
+# Path on GitHub / local workspace
+LOCAL_DIR = require_dir("LOCAL_DIR")
+BASELINE_METADATA_DIR = LOCAL_DIR / "results" / "metadata" / "baseline"
+
 # Clip folders
 UNLABELLED_CLIPS_DIR = ROOT_DIR / "cross_sucking_clips"
 LABELLED_CLIPS_DIR = ROOT_DIR / "cross_sucking_labelled"
@@ -31,7 +35,13 @@ LABELLED_CLIPS_DIR = ROOT_DIR / "cross_sucking_labelled"
 SOURCE_VIDEOS_DIR = ROOT_DIR / "raw_cross_sucking_datalog" / "videos"
 
 # Index
-INDEX_PATH = UNLABELLED_CLIPS_DIR / "all_clips_index.csv"
+INDEX_PATH = UNLABELLED_CLIPS_DIR / "all_clips_index.csv"  # Cloud Copy
+RAW_INDEX_PATH = (
+    LOCAL_DIR / "data" / "raw" / "data/raw/all_clips_index_raw.csv"
+)  # Local Copy
+PROCESSED_INDEX_PATH = (
+    LOCAL_DIR / "data" / "processed" / "processed_clips_index.csv"
+)  # Filtered Data
 
 # Data / metadata
 DATA_FOLDER_DIR = ROOT_DIR / "data"
@@ -41,7 +51,3 @@ METADATA_DIR.mkdir(parents=True, exist_ok=True)
 # Output clips
 REPRODUCED_CLIPS_DIR = ROOT_DIR / "reproduced_clips"
 REPRODUCED_CLIPS_DIR.mkdir(parents=True, exist_ok=True)
-
-# Path on GitHub / local workspace
-LOCAL_DIR = require_dir("LOCAL_DIR")
-BASELINE_METADATA_DIR = LOCAL_DIR / "results" / "metadata" / "baseline"
