5d373b4b952a6e4c6996fa4a6e7cee3b3bc3e83e
Devon Vorster <devo@telus.net>
2026-05-27 13:54:46 -0700

    Update script calling to match new repo organization

--- tests/matching/test_parse_unlabelled_names.py
+++ tests/matching/test_parse_unlabelled_names.py
@@ -13,7 +13,7 @@ root_dir = current_file_path.parent.parent.parent
 if str(root_dir) not in sys.path:
     sys.path.insert(0, str(root_dir))
 
-from scripts.matching import (
+from scripts.data_reading.matching import (
     parse_unlabelled_name,
 )
 
