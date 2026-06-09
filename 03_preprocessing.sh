b1f821a75985c0f039324a504c6c31c3dd28749d
Devon Vorster <devo@telus.net>
2026-06-08 19:51:18 -0700

    Update run time for preprocessing

--- 03_preprocessing.sh
+++ 03_preprocessing.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32gb
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --output=logs/preprocess_%A_%a.out
 #SBATCH --error=logs/preprocess_%A_%a.err
 
