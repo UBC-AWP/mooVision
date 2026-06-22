39fe00189b40a2247fd278e6acd42c0f5b21acb5
Cynthia Agata Limantono <cea@student.ubc.ca>
2026-06-21 23:40:43 -0700

    remove partition

--- 06_model_evaluation.sh
+++ 06_model_evaluation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=model-eval
 #SBATCH --account=st-nina-1
-#SBATCH --partition=interactive            # CPU-only, eval needs no GPU
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
