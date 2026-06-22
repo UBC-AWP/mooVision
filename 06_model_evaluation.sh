f7666f7d3998fce2ebb278dfc94da6f9e6b25d0c
Cynthia Agata Limantono <cea@student.ubc.ca>
2026-06-22 00:29:40 -0700

    add no-sync to debug sockeye evaluation

--- 06_model_evaluation.sh
+++ 06_model_evaluation.sh
@@ -67,7 +67,7 @@ fi
 # ==============================================================================
 # RUN
 # ==============================================================================
-uv run python scripts/evaluation.py \
+uv run --no-sync python scripts/evaluation.py \
     --predictions "${PREDICTIONS}" \
     --ground_truth "${GROUND_TRUTH}" \
     --output "${OUTPUT_FILE}" \
